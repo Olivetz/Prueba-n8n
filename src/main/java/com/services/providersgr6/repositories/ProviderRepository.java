507f3b1f112cf107bdd9740fb33dbaf866d478b8
olivet.chona@segurosbolivar.com-JuniorDeveloper <olivet.chona@segurosbolivar.com>
2025-10-30 10:37:11 -0500

    PN-2 Cambiar nombre metodo [auto-pr,main,tarea-completada]
    PN-2 Cambiar nombre metodo [auto-pr,main,tarea-completada]

--- src/main/java/com/services/providersgr6/repositories/ProviderRepository.java
+++ src/main/java/com/services/providersgr6/repositories/ProviderRepository.java
@@ -12,5 +12,5 @@ public interface ProviderRepository extends MongoRepository<Provider, String> {
     List<Provider> findByRegexName(String regexp);
 
     //@Query("{'name' : :#{#name}}")
-    //List<Provider> findByNamePrueba(@Param("name") String name);
+    //List<Provider> findByNamePrueba1(@Param("name") String name);
 }
