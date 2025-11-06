c44834f5fa9a96d621e915ed0e72c952e4d40d7c
olivet.chona@segurosbolivar.com-JuniorDeveloper <olivet.chona@segurosbolivar.com>
2025-11-06 15:52:20 -0500

    PN-2 se agrega comentariosssss [auto-pr,main,tarea-completada]

--- src/main/java/com/services/providersgr6/repositories/ProviderRepository.java
+++ src/main/java/com/services/providersgr6/repositories/ProviderRepository.java
@@ -12,5 +12,5 @@ public interface ProviderRepository extends MongoRepository<Provider, String> {
     List<Provider> findByRegexName(String regexp);
 
     //@Query("{'name' : :#{#name}}")
-    //List<Provider> findByNamePrueba9(@Param("name") String name);
+    //List<Provider> findByNamePrueba11(@Param("name") String name);
 }
